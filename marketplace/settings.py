c7c8c057864740cb9756b1ae7258e4f3949a6cf4
Олександр Олександрович <aleksvnikuda2008@gmail.com>
2024-08-16 12:38:05 +0300

    Static files settings

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -116,6 +116,11 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = BASE_DIR / "staticfiles"
+STATIC_DIRS = [
+    BASE_DIR / "static"
+]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
