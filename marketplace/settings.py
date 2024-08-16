4e5995866efbfd943d372f1412b392b5ff9594f1
Олександр Олександрович <aleksvnikuda2008@gmail.com>
2024-08-16 12:38:37 +0300

    Media files settings

--- marketplace/settings.py
+++ marketplace/settings.py
@@ -121,6 +121,9 @@ STATIC_DIRS = [
     BASE_DIR / "static"
 ]
 
+# Media files
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
