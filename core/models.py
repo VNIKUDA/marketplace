07e0d4a30a61d9e8ace423f51c2e069822ba31bf
Олександр Олександрович <aleksvnikuda2008@gmail.com>
2024-08-17 10:17:21 +0300

    Change core/models.py

--- core/models.py
+++ core/models.py
@@ -7,15 +7,21 @@ from hashlib import sha3_224
 from datetime import datetime
 
 def custom_upload_directory(instance, filename):
+    # Main directory of the upload (is the name of the parent model class)
     directory = instance.parent_type.model
 
+    # Subdirectory of the upload (is hashed version of the parent's id)
+    subderictory_hash = sha3_224()
+    subderictory_hash.update(f"{instance.parent_id}".encode())
+    subderictory = subderictory_hash.hexdigest()
+
+    # Hashed version of the upload file
     filename, filetype = os.path.splitext(filename)
-
     filename_hash = sha3_224()
-    filename_hash.update(f"{filename}{datetime.now()}".encode())
-    filename = filename_hash.hexdigest()
+    filename_hash.update(f"{filename}{datetime.now()}".encode()) # add current time to the name of 
+    filename = filename_hash.hexdigest()                         # the file and then hash it
 
-    return f"{directory}/{filename}{filetype}"
+    return f"{directory}/{subderictory}/{filename}{filetype}"
 
 # Create your models here.
 class UploadFile(models.Model):
